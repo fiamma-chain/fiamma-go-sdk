c0c76e9e8e796d27e82ebf9e5eb184318bcd7682
OkabeRintar0u <yibishufange@fiammachain.io>
2025-03-03 16:35:46 +0800

    comment context log

--- ginctx/context.go
+++ ginctx/context.go
@@ -260,7 +260,7 @@ func Wrapper(handler HandlerFunc) func(c *gin.Context) {
 		}()
 		res, err := handler(cc)
 		if err != nil {
-			cc.Logger.Info("failed to handler request", log.Any("error", err))
+			//cc.Logger.Info("failed to handler request", log.Any("error", err.Error()))
 			PopulateFailedResponse(cc, err, false)
 			return
 		}
